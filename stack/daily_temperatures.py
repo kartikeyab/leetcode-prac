12e4d6d1d6e8c832b6a2ad23e7d42c7e19507a47
Kartikeya <kartkeyabhardwaj98@gmail.com>
2021-03-23 20:11:43 +0530

    lil cheat

---
+++ stack/daily_temperatures.py
@@ -0,0 +1,32 @@
+"""
+For example, 
+given the list of temperatures 
+T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0]
+"""
+
+class Solution:
+    def dailyTemperatures(self, T):
+        pass
+
+
+s = Solution()
+print(s.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
+                    
+
+
+
+
+
+            
+
+
+
+
+
+
+
+        
+
+        
+
+        
