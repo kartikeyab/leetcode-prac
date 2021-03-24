4026525be18ba82fec2c35276fac7d5eafba7372
Kartikeya <kartkeyabhardwaj98@gmail.com>
2021-03-24 10:28:45 +0530

    daily_temp solved

--- stack/daily_temperatures.py
+++ stack/daily_temperatures.py
@@ -4,10 +4,17 @@ given the list of temperatures
 T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0]
 """
 
-class Solution:
+class Solution(object):
     def dailyTemperatures(self, T):
-        pass
-
+        ans = [0] * len(T)
+        stack = [] #indexes from hottest to coldest
+        for i in range(len(T) - 1, -1, -1):
+            while stack and T[i] >= T[stack[-1]]:
+                stack.pop()
+            if stack:
+                ans[i] = stack[-1] - i
+            stack.append(i)
+        return ans
 
 s = Solution()
 print(s.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
